a21005d8aa7967cbead09e1acc551485f7ce5d8c
Peter Geyser <peterpgeyser@gmail.com>
2019-11-01 20:02:35 -0700

    Added F1 logo to banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -5,8 +5,8 @@ function Banner(props) {
     return (
         <nav>
             <ul class="nav nav-fill">
-                <li class="nav-item">Score: {props.score}</li>
-                <li class="nav-item">Clicky Game</li>
+                <li class="nav-item">Score: {props.score}/20</li>
+                <li class="nav-item"><img src="./assets/images/f1_logo.svg" id="f1_logo" alt="F1 Logo"/> Clicky Game</li>
                 <li class="nav-item">Wins: {props.wins}</li>
             </ul>
         </nav>
