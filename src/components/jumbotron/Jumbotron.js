2256b73371804b7e951b0ff737ba499e0a889784
Peter Geyser <peterpgeyser@gmail.com>
2019-11-01 20:27:51 -0700

    Jumbotron complete

--- src/components/jumbotron/Jumbotron.js
+++ src/components/jumbotron/Jumbotron.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./styles.css";
 
-function Jumbotron(props) {
+function Jumbotron() {
     return (
         <div class="jumbotron jumbotron-fluid">
             <div class="container">
-                <h1 class="display-4">Fluid jumbotron</h1>
-                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                <h1 class="display-4">Formula 1 Clicky Game</h1>
+                <p class="lead">Click on a driver image to earn points, but don't click on any driver more than once!</p>
             </div>
         </div>
     )
