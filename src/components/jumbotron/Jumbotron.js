d7d154c9da9855787e4eeb974d4002eca1f1f7df
Peter Geyser <peterpgeyser@gmail.com>
2019-11-01 23:33:25 -0700

    Set jumbotron message to be a prop

--- src/components/jumbotron/Jumbotron.js
+++ src/components/jumbotron/Jumbotron.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./styles.css";
 
-function Jumbotron() {
+function Jumbotron(props) {
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
-                <h1 className="display-4">Formula 1 Clicky Game</h1>
+                <h1 className="display-4">{props.jumbotronMessage}</h1>
                 <p className="lead">Click on a driver image to earn points, but don't click on any driver more than once!</p>
             </div>
         </div>
