232352ca66e4051a8aa3ab9d886b16d94f6696b7
Peter Geyser <peterpgeyser@gmail.com>
2019-11-03 11:22:34 -0800

    Fixed pathing for f1 logo image path

--- src/components/jumbotron/Jumbotron.js
+++ src/components/jumbotron/Jumbotron.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./styles.css";
 
-function Jumbotron(props) {
+function Jumbotron() {
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
-                <h1 className="display-4"><img src="/assets/images/f1_logo.svg" className="pb-3" id="f1_logo" alt="f1 logo" /> Clicky Game</h1>
+                <h1 className="display-4"><img src="./assets/images/f1_logo.svg" className="pb-3" id="f1_logo" alt="f1 logo" /> Clicky Game</h1>
                 <p className="lead">Click on a driver image to earn points, but don't click on any driver more than once!</p>
             </div>
         </div>
